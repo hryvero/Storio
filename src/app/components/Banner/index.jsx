da7f4901de66b205f8456d049abe932784e9356a
hryvero <grigorivveronika@gmail.com>
2024-10-24 18:06:31 +0300

    feat: new section, code refactoring

--- src/app/components/Banner/index.jsx
+++ src/app/components/Banner/index.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import styles from './styles.module.scss'
+import React from "react";
+import styles from "./styles.module.scss";
 
-
-const Banner = (props) => {
-    return (
-        <div className={styles.banner}>
-            <h2 className={styles.banner__title }>{props.title}</h2>
-            <p className={styles.banner__description}>{props.description}</p>
-        </div>
-    );
-}
+const Banner = ({ bannerText, textLocation }) => {
+  return (
+    <div
+      className={
+        `${textLocation}` == "center" ? styles.banner_center : styles.banner
+      }
+    >
+      <h2
+        className={
+          `${bannerText.className}` == "large"
+            ? styles.banner__title_size_l
+            : styles.banner__title
+        }
+      >
+        {bannerText.title}
+      </h2>
+      <p className={styles.banner__description}>{bannerText.description}</p>
+    </div>
+  );
+};
 
 export default Banner;
--- src/app/page.js
+++ src/app/page.js
@@ -7,24 +7,51 @@ import { Fragment } from "react";
 import Info from "./components/Info/Info";
 
 export default function Home() {
-  const bannerText = {
-    title: (
-      <Fragment>
-        {" "}
-        Find <strong> all you need </strong> <br /> in <strong>Storio</strong>{" "}
-        platform{" "}
-      </Fragment>
-    ),
-    description: (
-      <Fragment>
-        {" "}
-        Listings from different fields - from furnitures to books and
-        electronics.
-        <br /> Sign up to see all the listings and find the best match for you.
-      </Fragment>
-    ),
-    className: "styles.banner__title_size_l",
-  };
+  const bannerText = [
+    {
+      id: 1,
+      title: (
+        <>
+          Find{" "}
+          <span>
+            <strong> all you need </strong>
+          </span>{" "}
+          <br /> in
+          <span>
+            {" "}
+            <strong> Storio </strong>
+          </span>
+          platform
+        </>
+      ),
+      description: (
+        <>
+          Listings from different fields - from furnitures to books and
+          electronics.
+          <br /> Sign up to see all the listings and find the best match for
+          you.
+        </>
+      ),
+      className: "large",
+    },
+    {
+      id: 2,
+      title: (
+        <>
+          Our awesome apps <br />
+          are loved worldwide
+        </>
+      ),
+      description: (
+        <>
+          We care about your users and we always pay huge attention to create a
+          <br />
+          product that people <strong>love</strong> to use every day.
+        </>
+      ),
+      className: "medium",
+    },
+  ];
 
   const imagesMock = [
     { id: 1, src: "/tablet.png", number: "500+", name: "Listings" },
@@ -36,20 +63,21 @@ export default function Home() {
     <main className={styles.main}>
       <Header actions={actions} />
       {/* BANNER */}
-      <div className="banner__section">
-        <div className="banner__section-block">
-          <Banner
-            title={bannerText.title}
-            description={bannerText.description}
-            className={bannerText.className}
-          />
+      <div className={styles.banner__section}>
+        <div className={styles.banner__section_block}>
+          <Banner bannerText={bannerText[0]} />
           <Button />
         </div>
         <img src="/illustration.png" />
       </div>
       {/* INFO */}
       <div className={styles.info__section}>
-        <Info details={imagesMock} text={"randommm"} />
+        <Info details={imagesMock} />
+      </div>
+      {/* BANNER */}
+      <div className={styles.location__section}>
+        <Banner bannerText={bannerText[1]} textLocation="center" />
+        <img src="/location.png" />
       </div>
     </main>
   );
